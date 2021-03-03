53223462f298a50d9390e5fb798b3a8201f8c7f4
Dhaya <154633+dhayab@users.noreply.github.com>
2021-03-03 15:26:02 +0100

    chore: update assets from source

--- update.sh
+++ update.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 TMP=$(mktemp -d)
-REPOSITORY="https://github.com/LAB-MI/attestation-couvre-feu-covid-19/"
+REPOSITORY="https://github.com/LAB-MI/attestation-deplacement-derogatoire-covid-19/"
 SCRIPTS=("src/js/pdf-util.js" "src/js/util.js")
 RESOURCES=("src/certificate.pdf")
 
-git clone $REPOSITORY $TMP
-for SCRIPT in ${SCRIPTS[@]}; do
+git clone $REPOSITORY "$TMP"
+for SCRIPT in "${SCRIPTS[@]}"; do
     FILE="$TMP/$SCRIPT"
-    echo "const Blob = require('node-blob'); $(cat $FILE)" > $FILE
-    cp $FILE utils
+    sed -i "1i const Blob = require('node-blob')\n" "$FILE"
+    cp "$FILE" utils
 done
-for RESOURCE in ${RESOURCES[@]}; do cp $TMP/$RESOURCE public; done
+for RESOURCE in "${RESOURCES[@]}"; do cp "$TMP/$RESOURCE" public; done
 
-rm -rf $TMP
+rm -rf "$TMP"
